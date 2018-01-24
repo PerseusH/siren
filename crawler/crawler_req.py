54e0158173875025bb16e4b4ee41948a6db012ef
PerseusH <584148102@qq.com>
2018-01-24 17:25:46 +0800

    update requests_crawler

--- crawler/crawler_req.py
+++ crawler/crawler_req.py
@@ -3,7 +3,9 @@
 
 
 import requests
-import time, random, json
+import time
+import random
+import json
 from bs4 import BeautifulSoup
 
 
@@ -11,47 +13,69 @@ today = time.strftime('%Y-%m-%d', time.localtime())
 GAMES_URL = 'https://www.zhibo8.cc/nba/json/%s.htm?key=%s' % (today, str(random.random()))
 
 
-def download(url):
-    return requests.get(url)
+class Games():
+    def __init__(self):
+        self.i = 0
+        self.games = self.game_list()
+        self.count = len(self.games)
 
 
-def parse_html(html):
-    soup = BeautifulSoup(html, 'lxml')
-    return soup
+    def __iter__(self):
+        return self
 
 
-def game_info(gid):
-    ginfo = {}
-
-    ginfo_url = 'https://news.zhibo8.cc/nba/%s/%s.htm' % (today, gid)
-    gifo_soup = parse_html(download(ginfo_url).content)
-    ginfo['title'] = gifo_soup.find('div', attrs={'class': 'title'}).h1.getText()
-    ginfo['brief1'] = gifo_soup.find('div', attrs={'class': 'content'}).find_all('p')[3].getText()
-    ginfo['brief2'] = gifo_soup.find('div', attrs={'class': 'content'}).find_all('p')[4].getText()
-
-    score_url = 'https://bifen4pc.qiumibao.com/json/%s/%s.htm' % (today, gid)
-    score = download(score_url).json()
-    sinfo = '%s %s:%s %s' % (
-            score['home_team'],
-            score['home_score'],
-            score['visit_score'],
-            score['visit_team']
-            )
-    ginfo['score'] = sinfo
-
-    return ginfo
+    def next(self):
+        if  self.i == self.count - 1:
+            raise StopIteration
+        else:
+            self.i += 1
+            return self.games[self.i]
 
 
+    def parse_html(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        return soup
+    
+    
+    def game_info(self, gid):
+        ginfo = {}
+    
+        ginfo_url = 'https://news.zhibo8.cc/nba/%s/%s.htm' % (today, gid)
+        gifo_soup = self.parse_html(self.download(ginfo_url).content)
+        ginfo['title'] = gifo_soup.find('div', attrs={'class': 'title'}).h1.getText()
+        ginfo['brief1'] = gifo_soup.find('div', attrs={'class': 'content'}).find_all('p')[3].getText()
+        ginfo['brief2'] = gifo_soup.find('div', attrs={'class': 'content'}).find_all('p')[4].getText()
+    
+        score_url = 'https://bifen4pc.qiumibao.com/json/%s/%s.htm' % (today, gid)
+        score = self.download(score_url).json()
+        sinfo = '%s %s:%s %s' % (
+                score['home_team'],
+                score['home_score'],
+                score['visit_score'],
+                score['visit_team']
+                )
+        ginfo['score'] = sinfo
+    
+        return ginfo
+    
+    
+    def game_list(self):
+        response = self.download(GAMES_URL).json()
+        games = filter(lambda i: i['title'].startswith('NBA常规赛'), response['video_arr'])
+
+        return [self.game_info(game['saishiid']) for game in games]
+
+
+    def download(self, url):
+        return requests.get(url)
+    
+    
 def ec(s):
-    return s.encode('utf-8')
+    return s.encode('utf-8')    
 
 
 if __name__ == '__main__':
-    data = download(GAMES_URL).json()
-    items = data['video_arr']
-    for i in filter(lambda i: i['title'].startswith('NBA常规赛'), items):
-        #print ec(i['saishiid']), ec(i['shortTitle']), i['updatetime'], i['url']
-        ginfo = game_info(i['saishiid'])
+    for ginfo in Games():
         print ec(ginfo['title'])
         print ec(ginfo['score'])
         print ec(ginfo['brief1'])
